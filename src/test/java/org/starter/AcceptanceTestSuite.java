8bb3b208333ddb94aab19cb16354adca2ec19e49
octocat <aydinserbest34@gmail.com>
2024-07-09 00:15:42 +0200

    added camelCase vs. upperCase to the runner class

--- src/test/java/org/starter/AcceptanceTestSuite.java
+++ src/test/java/org/starter/AcceptanceTestSuite.java
@@ -6,6 +6,7 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.SNIPPET_TYPE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
@@ -15,6 +16,8 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 //@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
 //        value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-reports.html")
+@ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "camelcase") // Snippet tipi CamelCase olarak ayarlanır
+// @ConfigurationParameter(key = SNIPPET_TYPE_PROPERTY_NAME, value = "underscore") // Snippet tipi Underscore olarak ayarlanır
 
 public class AcceptanceTestSuite {
 }
--- src/test/java/org/starter/stepdefinitions/CheckoutSteps.java
+++ src/test/java/org/starter/stepdefinitions/CheckoutSteps.java
@@ -6,15 +6,16 @@ import io.cucumber.java.en.When;
 
 public class CheckoutSteps {
     @Given("the price of a {string} is 40c")
-    public void the_price_of_a_is_40c(String string) {
+    public void thePriceOfAIs40c(String string) {
 
     }
     @When("I checkout {int} {string}")
-    public void i_checkout(Integer int1, String string) {
+    public void iCheckout(Integer int1, String string) {
 
     }
     @Then("the total price should be 40c")
-    public void the_total_price_should_be_40c() {
+    public void theTotalPriceShouldBe40c() {
 
     }
+
 }
