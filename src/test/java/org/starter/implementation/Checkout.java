3a45bd5955030d35eea68bc19e6193c18d7094fb
octocat <aydinserbest34@gmail.com>
2024-07-09 00:43:01 +0200

    updated stepDefinitions with TDD approach

---
+++ src/test/java/org/starter/implementation/Checkout.java
@@ -0,0 +1,10 @@
+package org.starter.implementation;
+
+public class Checkout {
+
+    public void addToBasket(int itemCount, String itemName) {
+    }
+    public int total() {
+        return 40;
+    }
+}
--- src/test/java/org/starter/stepdefinitions/CheckoutSteps.java
+++ src/test/java/org/starter/stepdefinitions/CheckoutSteps.java
@@ -3,18 +3,27 @@ package org.starter.stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.starter.implementation.Checkout;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CheckoutSteps {
-    @Given("the price of a {string} is 40c")
-    public void thePriceOfAIs40c(String string) {
+    int bananaPrice = 0;
+    Checkout checkout;
+    @Given("the price of a {string} is {int}c")
+    public void thePriceOfAIs40c(String name, int price) {
+        bananaPrice = price;
 
     }
     @When("I checkout {int} {string}")
-    public void iCheckout(Integer int1, String string) {
+    public void iCheckout(int itemCount, String itemName) {
+        checkout = new Checkout();
+        checkout.addToBasket(itemCount, itemName);
 
     }
-    @Then("the total price should be 40c")
-    public void theTotalPriceShouldBe40c() {
+    @Then("the total price should be {int}c")
+    public void theTotalPriceShouldBe40c(int totalPrice) {
+        assertThat(totalPrice).isEqualTo(checkout.total());
 
     }
 
