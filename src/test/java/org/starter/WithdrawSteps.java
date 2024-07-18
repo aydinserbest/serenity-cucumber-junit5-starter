77b544ef6c6058e01d8db8045e7dd0c64e75bbfd
octocat <aydinserbest34@gmail.com>
2024-07-18 23:00:17 +0200

    updated

--- src/test/java/org/starter/WithdrawSteps.java
+++ src/test/java/org/starter/WithdrawSteps.java
@@ -8,18 +8,19 @@ import static org.junit.Assert.assertEquals;
 
 public class WithdrawSteps {
     private int balance;
-    private int withdrawal;
 
-    //@Given("I have //$(\\d+) in my account") //without using ^
+    //@Given("^I have \\$(\\d+) in my account")
     @Given("I have ${int} in my account")
     public void i_have_in_my_account(int balance) {
+
         this.balance = balance;
+        System.out.println(balance);
     }
 
     @When("I choose to withdraw the fixed amount of ${int}")
     public void i_choose_to_withdraw_the_fixed_amount_of(int withdrawal) {
-        this.withdrawal = withdrawal;
         this.balance -= withdrawal;
+        System.out.println(balance);
     }
 
     @Then("the balance of my account should be ${int}")
